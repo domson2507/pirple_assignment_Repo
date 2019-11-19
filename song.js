cc2def7889897d839afcdb6ebb076fa46895f108
domson2507 <yawtweeny25@gmail.com>
2019-11-19 08:40:57 +0000

    changes in assignment

--- song.js
+++ song.js
@@ -1,3 +1,6 @@
+/*This is a javascript code displaying the attribute
+of my favorite song*/
+
 var artist = "Shatta Wale"
 var songTitle = "Enter the Net"
 var genre = "Dance Hall"
@@ -7,3 +10,23 @@ var numberOfAwardsWon = 1;
 var yearOfRelease = 2018;
 var albumName = "single"
 var likedBy = ["DJ Black", "DJ Mic-Smith", "DJ Vyrusky", "DJ Sly", "DJ Aroma"];
+
+console.log(artist);
+console.log(songTitle);
+console.log(genre);
+console.log(durationInSeconds);
+console.log(hasWonAwards);
+console.log(yearOfRelease);
+console.log(yearOfRelease);
+console.log(albumName);
+console.log(likedBy);
+
+//artist describes the artist who sang my favorite song
+//songTitle describe the song title of my favorite song
+//genre describes the genre of my favorite song
+//durationInSeconds describes how long the song takes when being played in seconds
+//hasWonAwards is a boolean that tells if the song has won an award
+//numberOfAwrds decribes the number of awards won
+//yearOfRelease displays the year the song was released
+//albumName describes the album in which my favorite song can be found
+//likedby describes the number of DJ gave a thumbs up to the song
